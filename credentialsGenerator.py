b25dd8564782dff76557656016e2598504440a96
rolandogarci4 <rol.garcia@duocuc.cl>
2023-05-09 15:37:21 -0400

    First commit and credentialsGenerator method.

---
+++ credentialsGenerator.py
@@ -0,0 +1,14 @@
+
+
+de credentialsGenerator(txt, complement, year):
+    name = txt[0:3].lower() + '.' + complement.lower()
+    pwd = complement.lower + str(year)
+    return name, pwd
+
+class Person1:
+    nombre = 'John'
+    ap_paterno = 'Doe'
+    year = 2020
+                
+autoUsername = credentialsGenerator(Person.nombre Person1.ap_paterno, Person1.year)
+print('auto-username: ', autoUsername)
